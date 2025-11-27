8ca5f9c36f480e6ae073dec90bdbf8d80a8b77c4
biamarochi <163007667+biamarochi@users.noreply.github.com>
2025-11-27 18:35:53 -0300

    .

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Botão do menu (versão mobile)
 let menu = document.querySelector('#menu-btn');
 let navbar = document.querySelector('.navbar');
 
@@ -6,36 +7,39 @@ menu.onclick = () => {
     navbar.classList.toggle('active');
 };
 
+// Fechar menu ao rolar a página
 window.onscroll = () => {
     menu.classList.remove('fa-times');
     navbar.classList.remove('active');
 };
 
-document.querySelectorAll('.image-slider img').forEach(images => {
-    images.onclick = () => {
-        var src = images.getAttribute('src');
-        document.querySelector('.main-home-image').src = src;
+// Troca de imagem da seção "Início"
+document.querySelectorAll('.image-slider img').forEach(img => {
+    img.onclick = () => {
+        let src = img.getAttribute('src');
+        document.querySelector('.main-inicio-image').src = src;
     };
 });
 
+// Slider das matérias / avaliações
 var swiper = new Swiper(".review-slider", {
     spaceBetween: 20,
+
     pagination: {
         el: ".swiper-pagination",
         clickable: true,
     },
+
     loop: true,
     grabCursor: true,
+
     autoplay: {
         delay: 7500,
         disableOnInteraction: false,
     },
+
     breakpoints: {
-        0: {
-            slidesPerView: 1
-        },
-        768: {
-            slidesPerView: 2
-        }
+        0: { slidesPerView: 1 },
+        768: { slidesPerView: 2 },
     },
 });
